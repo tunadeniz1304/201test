5924a14830dad5551f23dcaf7b29312a2442b64e
tunadeniz1304 <tunadeniz1304@gmail.com>
2024-12-22 12:31:24 +0300

    6th commit

--- Main.java
+++ Main.java
@@ -3,61 +3,54 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Node {
- public char c;
- public Node left, right;
- public Node(char c) {
- this.c = c;
- this.left = this.right = null;
- }
-}
+    public char c;
+    public Node left, right;
 
+    public Node(char c) {
+        this.c = c;
+        this.left = this.right = null;
+    }
+}
 
 class Main {
- public static void main( String[] args ){
- Node root = buildTree( "43-12+*" );
- printTree( root );
- }
- public static Node buildTree( String postfixFormula ) {
- // write your implementation of buildTree here
- Stack<Node> stack = new Stack<Node>();
+    public static void main(String[] args) {
+        Node root = buildTree("43-12+*");
+        printTree(root);
+    }
 
+    public static Node buildTree(String postfixFormula) {
+        // write your implementation of buildTree here
+        Stack<Node> stack = new Stack<Node>();
 
- 
-for(int i =0; i< postfixFormula.length();++i){
- char c = postfixFormula.charAt(i);
-if (c == '+' || c== '-' || c == '*' || c == '/'){
-    //* çıktı  + çıktı 1 2 çıktı - den de 4 3 çıktı 
-Node node = new Node(c);
-Node right = stack.pop();
-Node left = stack.pop();
-node.right = right;
-node.left = left;
-stack.push(node);
-}else{
-Node node = new Node(c);
-stack.push(node);
-}
+        for (int i = 0; i < postfixFormula.length(); ++i) {
+            char c = postfixFormula.charAt(i);
+            if (c == '+' || c == '-' || c == '*' || c == '/') {
+                // * çıktı + çıktı 1 2 çıktı - den de 4 3 çıktı
+                Node node = new Node(c);
+                Node right = stack.pop();
+                Node left = stack.pop();
+                node.right = right;
+                node.left = left;
+                stack.push(node);
+            } else {
+                Node node = new Node(c);
+                stack.push(node);
+            }
 
+        }
 
+        return stack.pop();
 
-}
+    }
 
-return stack.pop();
-
-
-
-
-}
-
-public static void  printTree( Node root ) {
-if (root == null) {
+    public static void printTree(Node root) {
+        if (root == null) {
             return;
         }
- Queue<Node> queue = new LinkedList<Node>();
+        Queue<Node> queue = new LinkedList<Node>();
         queue.add(root);
-       
 
-while (queue.size()>0) {
+        while (queue.size() > 0) {
             int level = queue.size();
             for (int i = 0; i < level; i++) {
                 Node node = queue.remove();
@@ -74,5 +67,5 @@ while (queue.size()>0) {
             System.out.println();
         }
 
-}   
+    }
 }
